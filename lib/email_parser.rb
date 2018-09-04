7ec65e0ae474ca163645aa485048eeb3a8008570
Thomas Pope <tpope0928@gmail.com>
2018-09-04 12:57:11 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,21 @@ class EmailParser
   end
   
   def parse
-    
+    puts splitEmail = emails.split.collect {|address| address.split(',')} 
+    returnArr = splitEmail.flatten.uniq
+    returnArr
+  end
+
+end
+
+emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
+parser1 = EmailParser.new(emails1)
+parser1.parse 
+
+emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
+parser2 = EmailParser.new(emails2)
+parser2.parse
+
 
 
 
