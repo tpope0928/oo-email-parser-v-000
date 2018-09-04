ff590fe0b927fa8b1f66cb6d2709774d9798ad9a
Thomas Pope <tpope0928@gmail.com>
2018-09-04 12:57:43 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -16,15 +16,13 @@ class EmailParser
     returnArr
   end
 
-end
+  emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
+  parser1 = EmailParser.new(emails1)
+  parser1.parse 
 
-emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
-parser1.parse 
-
-emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
-parser2.parse
+  emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
+  parser2 = EmailParser.new(emails2)
+  parser2.parse
 
 
 
